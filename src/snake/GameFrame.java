c70d44cbdb6e0258cf31e89cf0f4cb815ced7ef1
Dragomir Petrov <dragomirpetrov7@gmail.com>
2022-12-29 11:28:08 +0200

    Created game classes and implemented needed frames

---
+++ src/snake/GameFrame.java
@@ -0,0 +1,6 @@
+package snake;
+
+import javax.swing.*;
+
+public class GameFrame extends JFrame {
+}
--- src/snake/SnakeGame.java
+++ src/snake/SnakeGame.java
@@ -1,4 +1,6 @@
-public class Main {
+package snake;
+
+public class SnakeGame {
     public static void main(String[] args) {
 
     }
